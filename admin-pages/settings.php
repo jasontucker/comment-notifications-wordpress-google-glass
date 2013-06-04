fc150074406923170dd4cd9f19db9255c3ab38ab
brandondove <brandon@pixeljar.net>
2013-06-03 19:42:38 -0700

    Finished up settings page HTML (issue #1)
    - Added form to wrap inputs
    - Added submit button
    - Changed slugs for clarity

--- admin-pages/settings.php
+++ admin-pages/settings.php
@@ -2,5 +2,9 @@
 <div class="wrap">
 	<?php screen_icon('options-general') ?><h2><?php _e( 'Google Glass Settings' ); ?></h2>
 
-	<?php do_settings_sections( 'ggc_settings' ) ?>
+	<form method="post" action="options.php">
+		<?php settings_fields( 'ggc-settings' ); ?>
+		<?php do_settings_sections( 'google-glass-comments' ) ?>
+		<?php submit_button( __( 'Save Settings' ) ) ?>
+	</form>
 </div>
