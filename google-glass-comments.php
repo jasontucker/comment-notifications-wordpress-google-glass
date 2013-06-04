869775ac0ded2655a0831110d353a5d5174a9810
brandondove <brandon@pixeljar.net>
2013-06-04 00:53:29 -0700

    Create some constants to hold paths, etc. use paths in require statements

--- google-glass-comments.php
+++ google-glass-comments.php
@@ -30,9 +30,26 @@ Author URI: http://ocwp.org/
  * **********************************************************************
  */
 
-require_once( 'lib/hooks.php' );
-require_once( 'lib/settings.php' );
-require_once( 'lib/notifications.php' );
+// SET UP PATH CONSTANTS
+define( 'GGC',					'google-glass-comments' );
+define( 'GGC_VERSION',			'1.0' );
+define( 'GGC_URL',				plugin_dir_url( __FILE__ ) );
+define( 'GGC_ABS',				plugin_dir_path( __FILE__ ) );
+define( 'GGC_SLUG',				plugin_basename( __FILE__ ) );
+define( 'GGC_REL',				basename( dirname( __FILE__ ) ) );
+define( 'GGC_LANG',				GGC_ABS.'i18n/' );
+define( 'GGC_ADMIN_PAGES',		GGC_ABS.'admin-pages/' );
+define( 'GGC_LIB',				GGC_ABS.'lib/' );
+define( 'GGC_CSS',				GGC_URL.'css/' );
+define( 'GGC_IMG',				GGC_URL.'images/' );
+define( 'GGC_JS',				GGC_URL.'js/' );
+
+// INTERNATIONALIZATION
+load_plugin_textdomain( GGC, null, GGC_REL );
+
+require_once( GGC_LIB.'hooks.php' );
+require_once( GGC_LIB.'settings.php' );
+require_once( GGC_LIB.'notifications.php' );
 
 /**
  * Create the settings page
@@ -42,5 +59,5 @@ function ggc_create_admin_menus() {
 	add_comments_page( 'Google Glass', 'Google Glass', 'edit_posts', 'google-glass-comments', 'ggc_settings_page' );
 }
 	function ggc_settings_page() {
-		require_once( 'admin-pages/settings.php' );
+		require_once( GGC_ADMIN_PAGES.'settings.php' );
 	}
