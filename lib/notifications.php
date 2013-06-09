b9b7bf58ba37b9b1c2b404a3f27af67681831128
brandondove <brandon@pixeljar.net>
2013-06-09 01:58:52 -0700

    Grab oauth settings, pass the access token with the request. Slim down on what we're sending to google. Actually let the user delete the timeline card (whoops). Store the resulting timeline object as comment meta since we'll need a reference to it when the user takes action.

--- lib/notifications.php
+++ lib/notifications.php
@@ -10,29 +10,16 @@ function ggc_send_notification( $id = 0, $comment = array() ) {
 
 	$datetime = date( DATE_RFC3339 );
 	$options = ggc_get_options();
+	$oauth = get_option( 'ggc-oauth-settings' );
 
 	$result = wp_remote_post(
-		'https://www.googleapis.com/upload/mirror/v1/timeline',
+		add_query_arg( array( 'access_token' => $oauth['oauth-access-token'] ), 'https://www.googleapis.com/mirror/v1/timeline' ),
 		array(
 			'body' => json_encode(array(
-				"key"			=> $options['api-simple-key'],
-				"kind"			=> "mirror#timelineItem",
-				"id"			=> "ggc-{$id}",
+				"bundleId"		=> sprintf( "ggc-%s-items", sanitize_title( get_bloginfo( 'blogname' ) ) ),
 				"sourceItemId"	=> $id,
 				"canonicalUrl"	=> get_comment_link( $comment ),
-				"bundleId"		=> sprintf( "ggc-%s-items", sanitize_title( get_bloginfo( 'blogname' ) ) ),
-				"isBundleCover"	=> false,
-				"selfLink"		=> get_comment_link( $comment ),
-				"created"		=> $datetime,
-				"updated"		=> $datetime,
-				"displayTime"	=> $datetime,
-				"isPinned"		=> false,
-				"isDeleted"		=> false,
-				"inReplyTo"		=> "ggc-{$comment->comment_parent}",
-				"title"			=> apply_filters( 'ggc-card-title', sprintf( __( 'New Comment for "%s" on %s' ), get_the_title( $comment->comment_post_ID ), get_bloginfo( 'blogname' ) ), $id, $comment ),
-				"text"			=> apply_filters( 'ggc-card-text', sprintf( __( '%s said "%s" on "%s"'), get_comment_author( $id ), get_comment_text( $id ), get_the_title( $comment->comment_post_ID ) ), $id, $comment ),
-				"html"			=> apply_filters( 'ggc-card-html', sprintf( '<article><section><h1 class="text-normal blue">%s</h1><hr><p class="text-x-small">%s</p></section><footer><p><img class="left icon-small" src="%s">%s</p></footer></article>', __( 'New Comment' ), get_comment_text( $id ), get_avatar( get_comment_author_email( $id ), '100' ), get_comment_author( $id ) ), $id, $comment ),
-				"htmlPages"		=> array( '' ),
+				"html"			=> apply_filters( 'ggc-card-html', sprintf( '<article><section><h1 class="text-normal blue">%s</h1><hr><p class="text-x-small">%s</p></section><footer><p>%s%s</p></footer></article>', __( 'New Comment' ), get_comment_text( $id ), str_replace( "class='", "class='left icon-small ", get_avatar( get_comment_author_email( $id ), '100' ) ), get_comment_author( $id ) ), $id, $comment ),
 				"speakableText"	=> apply_filters( 'ggc-card-spoken-text', get_comment_text( $id ), $id, $comment ),
 				"menuItems"		=> array(
 					array(
@@ -58,6 +45,22 @@ function ggc_send_notification( $id = 0, $comment = array() ) {
 							"displayName"	=> "Delete",
 							"iconUrl"		=> GGC_IMG."delete.png" // http://thenounproject.com/noun/trash/#icon-No16505 Trash, designed by filip (http://thenounproject.com/fmal) from The Noun Project
 						))
+					),
+					array(
+						"action"	=> "DELETE",
+						"id"		=> "delete_card",
+						"values"	=> array(array(
+							"displayName"	=> "Delete Card",
+							"iconUrl"		=> GGC_IMG."delete.png" // http://thenounproject.com/noun/trash/#icon-No16505 Trash, designed by filip (http://thenounproject.com/fmal) from The Noun Project
+						))
+					),
+					array(
+						"action"	=> "READ_ALOUD",
+						"id"		=> "read_card",
+						"values"	=> array(array(
+							"displayName"	=> "Read Card",
+							"iconUrl"		=> GGC_IMG."delete.png" // http://thenounproject.com/noun/trash/#icon-No16505 Trash, designed by filip (http://thenounproject.com/fmal) from The Noun Project
+						))
 					)
 				),
 				"notification"=> array(
@@ -65,16 +68,16 @@ function ggc_send_notification( $id = 0, $comment = array() ) {
 				)
 			)), // json_encode
 			'headers' => array(
-				'Content-Type'	=> 'application/json'
+				'Content-Type'	=> 'application/json',
+				'Authorization'	=> 'Bearer '.$oauth['oauth-access-token']
 			)
 		)
 	); // wp_remote_post
 
 	if( is_wp_error( $result ) ) :
 		error_log( sprintf( __( 'Google Glasses Comments Notification Error: %s' ), print_r( array( 'id' => $id, 'comment' => $comment, 'result' => $result ), true ) ) );
+		return false;
 	endif;
 
-	wp_mail( 'brandon@pixeljar.net', 'comment from glass', sprintf( __( 'Google Glasses Result: %s' ), print_r( array( 'id' => $id, 'comment' => $comment, 'result' => $result ), true ) ) );
-	error_log( sprintf( __( 'Google Glasses Result: %s' ), print_r( array( 'id' => $id, 'comment' => $comment, 'result' => $result ), true ) ) );
-
+	add_comment_meta( $id, 'ggc-object', json_decode( wp_remote_retrieve_body( $result ) ), true );
 }
