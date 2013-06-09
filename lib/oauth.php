ce2ac1cfcca4612f75c4565e3d0206fc61df7eb8
brandondove <brandon@pixeljar.net>
2013-06-08 21:46:14 -0700

    oAuth
    -send ggc-oauth-setup as the state
    - on admin_init, listen for the oauth response
    	- die on error
    	- request access token on success
    - print out the response token

--- lib/oauth.php
+++ lib/oauth.php
@@ -1,11 +1,24 @@
 <?php
 
 // ggc-oauth-setup
+add_action( 'admin_init', 'ggc_catch_oauth_response' );
 function ggc_catch_oauth_response() {
-	if( isset( $_REQUEST['step'] ) && $_REQUEST['step'] == 'ggc-oauth-setup' ) :
 
-		die( _pring_r( $_REQUEST ) );
+	$screen = get_current_screen();
+	if( $screen->id != 'comments_page_google-glass-comments' )
+		return;
 
+	if( isset( $_REQUEST['state'] ) && $_REQUEST['state'] == 'ggc-oauth-setup' ) :
+
+		// Crap
+		if( isset( $_REQUEST['error'] ) ) :
+			wp_die( __( 'OH NOES!' ) );
+
+		// SA-WEET-AH!
+		elseif( isset( $_REQUEST['code'] ) ) :
+			ggc_get_access_token( $_REQUEST['code'] );
+
+		endif;
 
 	endif;
 }
@@ -26,11 +39,42 @@ function gcc_get_oauth_link() {
 			'client_id'			=> $options['api-client-id'],
 			'redirect_uri'		=> add_query_arg( array( 'page' => 'google-glass-comments', 'step' => 'ggc-oauth-setup' ), admin_url( 'edit-comments.php' ) ),
 			'scope'				=> 'https://www.googleapis.com/auth/glass.timeline',
-			'state'				=> get_current_user_id(),
+			'state'				=> 'ggc-oauth-setup',
 			'access_type'		=> 'online',
-			'approval_prompt'	=> 'force',
+			'approval_prompt'	=> 'force'
 		),
 		"https://accounts.google.com/o/oauth2/auth"
 	);
 	return $link;
+}
+
+function ggc_get_access_token( $code = '' ) {
+	$options = ggc_get_options();
+
+	$result = wp_remote_post(
+		"https://accounts.google.com/o/oauth2/token",
+		array(
+			'body' => http_build_query(array(
+				'code'			=> $code,
+				'client_id'		=> $options['api-client-id'],
+				'client_secret'	=> $options['api-client-secret'],
+				'redirect_uri'	=> add_query_arg( array( 'page' => 'google-glass-comments', 'step' => 'ggc-oauth-setup' ), admin_url( 'edit-comments.php' ) ),
+				'grant_type'	=> 'authorization_code'
+			)),
+			'headers' => array(
+				'Content-Type'	=> 'application/x-www-form-urlencoded'
+			)
+		)
+	);
+
+	if( is_wp_error( $result ) ) :
+		error_log( sprintf( __( 'Google Authentication Error: %s' ), print_r( array( 'result' => $result ), true ) ) );
+	endif;
+
+	wp_mail( 'brandon@pixeljar.net', 'google authentication', sprintf( __( 'Google Glasses Result: %s' ), print_r( array( 'result' => $result ), true ) ) );
+	error_log( sprintf( __( 'Google Authentication Result: %s' ), print_r( array( 'result' => $result ), true ) ) );
+
+	$result_body = wp_remote_retrieve_body( $result );
+	echo '<pre>'.print_r( json_decode( $result_body ), true ).'</pre>';
+
 }
